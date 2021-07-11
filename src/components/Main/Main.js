232a777de2e9f385ac272a19ce1f3edbd763fbe9
Srikar.Rama <srikarrama555@gmail.com>
2021-07-11 13:53:33 +0000

    updated

---
+++ src/components/Main/Main.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Main.css'
+import withSizes from "react-sizes";
+import { RiDeleteBin5Fill } from 'react-icons/ri'
+function Main(props) {
+    return (
+        <div className="row p-2" style={{ justifyContent: "space-around"}}>
+            <div className="col-12 col-md-7 links p-2" style={{ height: props.winHeight, display: "flex", justifyContent: "center" }}>
+                <div className="card col-11 p-2 meetinglink" style={{ height: "fit-content" }}>
+                    <h4>Title of the meet</h4>
+                    <div style={{display:"flex",justifyContent:"space-around"}}>
+                        <button className="btn btn-primary col-5">Join</button>
+                        <button className="btn btn-danger col-5">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-12 col-md-4 sform" style={{ height: props.windowHeight }}>
+                <form>
+                    <fieldset disabled>
+                        <legend>Create Meeting</legend>
+                        <div class="mb-3">
+                            <label class="form-label">Meeting Title</label>
+                            <input type="text" class="form-control" placeholder="title"></input>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Description</label>
+                            <input type="text" class="form-control" placeholder="description"></input>
+                        </div>
+                        <div class="mb-3">
+                            <label for="disabledSelect" class="form-label">which meeting?</label>
+                            <select id="Select" class="form-select">
+                                <option>Google meet</option>
+                                <option>Zoom</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Create</button>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    )
+}
+const mapSizesToProps = ({ height, width }) => ({
+    winHeight: height,
+    winWidth: width,
+});
+export default withSizes(mapSizesToProps)(Main);
